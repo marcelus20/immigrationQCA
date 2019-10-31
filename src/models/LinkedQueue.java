338ee04d5614ffeba17b53c5c032f75201dc8511
marcelus <marcelus20felipe@gmail.com>
2019-10-31 04:20:49 +0000

    Recreated Node class and refactored LinkedQueue class

--- src/models/LinkedQueue.java
+++ src/models/LinkedQueue.java
@@ -4,153 +4,56 @@ package models;
 import java.util.NoSuchElementException;
 
 /**
- * Class to keep track of the front (front) and rear (rear) of a doubly link of nodes
+ * Class to keep track of the before (before) and after (after) of a doubly link of nodes
  * @param <T>
  */
-public class LinkedQueue<T> implements QueueInterface<T>{
+public class LinkedQueue<T> {
 
     /**
-     * Atribute list: front, rear, size
+     * Atribute list: before, after, size
      */
-    private Node<T> front;
-    private Node<T> rear;
-    private int size;
+    protected Node<T> head;
+    protected Node<T> rear;
+    protected int size;
 
     public LinkedQueue() {
         size = 0;
     }
 
-    /**
-     * getters
-     * @return
-     */
-    public Node<T> getFront() {
-        return front;
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public void enqueue(T element){
+        if(isEmpty()) head = rear =  new Node<>(element);
+        else{
+            Node<T> newRear = new Node<>(element);
+            newRear.setPrevious(rear);
+            rear.setNext(newRear);
+            rear = newRear;
+        }
+        size++;
+    }
+
+    public T dequeue(){
+        if(isEmpty()) throw new NoSuchElementException();
+        T toReturn = head.getElement();
+        head = head.getNext();
+        if(size > 1) head.setPrevious(null);
+        else rear = head;
+        size--;
+        return toReturn;
+    }
+
+    public Node<T> getHead() {
+        return head;
     }
 
     public Node<T> getRear() {
         return rear;
     }
 
-
-    /**
-     * keep track of the size. Complexity is O(1)
-     * @return
-     */
-    @Override
-    public int size() {
+    public int getSize() {
         return size;
-    }
-
-    /**
-     * check whether queue is empty by looking at size and value of the front attribute
-     * Complexit O(3)
-     * @return
-     */
-    @Override
-    public boolean isEmpty() {
-        return size <= 0 && front == null;
-    }
-
-    /**
-     *
-     * @return
-     */
-    @Override
-    public T front() {
-        return front.getElement();
-    }
-
-    @Override
-    public T rear() {
-        return rear.getElement();
-    }
-
-
-    /**
-     * adds a next node to the current rear, making it a new rear
-     * @param element
-     */
-    @Override
-    public void enqueue(T element) {
-        if(isEmpty()){
-            enqueueWhenFrontIsNull(element);
-        }else if(front.getNextNode() == null){
-            enqueueWhenFrontNextNodeIsNull(element);
-        }else{
-            Node<T> last = rear;
-            rear = Node.nodeFactory(element);
-            rear.setPreviousNode(last);
-            last.setNextNode(rear);
-        }
-        size++;
-    }
-
-    /**
-     * Helper for enqueue. Responsability - enqueue when front.getNextNode() is null
-     * @param element
-     */
-    private void enqueueWhenFrontNextNodeIsNull(T element) {
-        Node<T> newNode = Node.nodeFactory(element);
-        newNode.setPreviousNode(front);
-        front.setNextNode(newNode);
-        rear = newNode;
-    }
-
-    /**
-     * Helper for enqueue. Responsability - enqueue when front is null
-     * @param element
-     */
-    private void enqueueWhenFrontIsNull(T element) {
-        front = Node.nodeFactory(element);
-        rear = front;
-    }
-
-
-    /**
-     * Updates the front of the queue to front.getNextNode() and gets rid of the old front.
-     * @return
-     */
-    @Override
-    public T dequeue() {
-        if (isEmpty()){//there is not element to return
-            throw new NoSuchElementException();
-        }else if(front.getNextNode() != null){
-            size--;
-            return dequeueWhenFrontNextNodeNotNull();
-        }else{
-            size--;
-            return dequeueWhenFrontNextNodeIsNull();
-        }
-    }
-
-    /**
-     * Helper for dequeue. Responsability - dequeue when front.getNextNode() is null
-     * @return
-     */
-    private T dequeueWhenFrontNextNodeIsNull() {
-        T item = front.getElement();
-        front = null;
-        return item;
-    }
-
-    /**
-     * Helper for dequeue. Responsability - dequeue when front.getNextNode() is not null
-     * @return
-     */
-
-    private T dequeueWhenFrontNextNodeNotNull() {
-        T item = front.getElement();
-        front = front.getNextNode();
-        front.setPreviousNode(null);
-        return item;
-    }
-
-    @Override
-    public String toString() {
-        return "LinkedQueue{" +
-                "front=" + front.getElement() +
-//                ", rear=" + rear.getElement() +
-                '}';
     }
 }
--- src/models/Node.java
+++ src/models/Node.java
@@ -1,193 +1,53 @@
 package models;
-import java.util.Objects;
 
-/**
- * This class is a node that stores a reference to the next node and
- * previous node. It is the doubly link list based node.
- * @param <T>
- */
-public class Node<T>{
-
-    /**
-     * List of attributes
-     */
-    private Node<T> previousNode;
+public class Node<T> {
+    private Node<T> previous;
     private T element;
-    private Node<T> nextNode;
+    private Node<T> next;
 
-
-    /**
-     * Factory for the node.
-     * @param element
-     * @param <T>
-     * @return
-     */
-    public static <T> Node <T> nodeFactory(T element){
-        return new Node<T>(element);
+    public static <T> void swapElements(Node<T> node1, Node<T> node2){
+        T tempElement = node1.getElement();
+        node1.setElement(node2.getElement());
+        node2.setElement(tempElement);
     }
 
-    /**
-     * Complete factory
-     * @param nextNode
-     * @param previousNode
-     * @param element
-     * @param <T>
-     * @return
-     */
-    public static <T> Node <T> nodeFactory(Node<T> previousNode, T element, Node<T> nextNode){
-        return new Node(previousNode, element, nextNode);
-    }
-
-    /**
-     * Factory that takes a node element
-     * @param node
-     * @param <T>
-     * @return
-     */
-    public static <T> Node <T> nodeFactory(Node<T> node){
-        return new Node<T>(node);
-    }
-
-
-    public static <T> Node <T> nodeFactory(){
-        return new Node<T>();
-    }
-
-
-    /**
-     * Constructor 1
-     * @param nextNode
-     * @param previousNode
-     * @param element
-     */
-    private Node(Node<T> previousNode, T element, Node<T> nextNode) {
-        this.previousNode = previousNode;
+    public Node(Node<T> previous, T element, Node<T> next) {
+        this.previous = previous;
         this.element = element;
-        this.nextNode = nextNode;
+        this.next = next;
     }
 
-
-    /**
-     * Constructor 2
-     * @param element
-     */
-    private Node(T element) {
-        this.previousNode = null;
+    public Node(T element, Node<T> next) {
         this.element = element;
-        this.nextNode = null;
+        this.next = next;
     }
 
-
-    private Node(){
-        previousNode = null;
-        element = null;
-        nextNode = null;
+    public Node(T element) {
+        this.element = element;
     }
 
-    /**
-     * Constructor 3
-     * @param node
-     */
-    private Node (Node<T> node){
-        previousNode = node.getPreviousNode();
-        element = node.getElement();
-        nextNode = node.getNextNode();
+    public Node<T> getPrevious() {
+        return previous;
     }
 
-
-    public Node <T> addNext(Node<T> node){
-        return new Node<>(this, node.getElement(), null);
-    }
-
-    public Node <T> addNext(T element){
-
-        return new Node<>(this, element, null);
-    }
-
-
-
-
-    /**
-     * getters
-     * @return
-     */
-    public Node<T> getNextNode() {
-        return nextNode;
-    }
-
-    public Node<T> getPreviousNode() {
-        return previousNode;
+    public void setPrevious(Node<T> previous) {
+        this.previous = previous;
     }
 
     public T getElement() {
         return element;
     }
 
-
-    public void setPreviousNode(Node<T> previousNode) {
-        this.previousNode = previousNode;
-    }
-
     public void setElement(T element) {
         this.element = element;
     }
 
-    public void setNextNode(Node<T> nextNode) {
-        this.nextNode = nextNode;
+    public Node<T> getNext() {
+        return next;
     }
 
-    /**
-     * Overriding equals and hashcode
-     * @param o
-     * @return
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node<?> node = (Node<?>) o;
-        return Objects.equals(nextNode, node.nextNode) &&
-                Objects.equals(previousNode, node.previousNode) &&
-                Objects.equals(element, node.element);
+    public void setNext(Node<T> next) {
+        this.next = next;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(nextNode, previousNode, element);
-    }
-
-
-    @Override
-    public String toString() {
-        try{
-            return "Node:::[" +
-                    "previous: " + previousNode.getElement() + "," +
-                    "element: " + element + "," +
-                    " next: " + nextNode.getElement() + "," +
-                    "]";
-        }catch (NullPointerException e){
-            if(nextNode == null && previousNode == null){
-                return "Node:::[" +
-                        "previous: none, "  +
-                        "element: " + element + "," +
-                        " next: none"  +
-                        "]";
-            }else if(nextNode == null){
-                return "(Node:::[" +
-                        "previous: "  +previousNode +
-                        "element: " + element + "," +
-                        " next: none" +
-                        "]";
-            }else if (previousNode == null){
-                return "Node:::[" +
-                        "previous: none " +
-                        "element: " + element + "," +
-                        " next: " + nextNode.getElement() +
-                        "])";
-            }else{
-                return null;
-            }
-        }
-
-    }
 }
